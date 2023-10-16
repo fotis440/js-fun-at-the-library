7e23ec0c9d4f5b6c2a7317c1b0fc8a5de5742b52
Fotis Kousiakis <fkousiakis@gmail.com>
2023-10-15 20:02:22 -0500

    3rdAttempt

--- src/book.js
+++ src/book.js
@@ -1,12 +1,51 @@
-function createTitle() {
+function createTitle(title) {
+  return "The " + title
   
 }
 
-module.exports = {
-  createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name, 
+    age: age,
+    pronouns: pronouns
+  };
+  }
+
+
+  function saveReview(reviews, reviewsArray) {
+    if (!reviewsArray.includes(reviews))
+    reviewsArray.push(reviews);
+    
+  }
+
+function calculatePageCount(title) {
+  return title.length * 20}
+
+
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
 }
+
+function editBook(book) {
+book.pageCount = book.pageCount * 0.75}
+
+
+
+
+
+
+  module.exports = {
+  createTitle,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
+}
+
